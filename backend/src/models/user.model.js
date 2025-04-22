163f1854a790bf88148cae2f7462744f77f5bca6
HulkyZeus <u20212200102@usco.edu.co>
2025-04-22 14:03:23 -0500

    avances users

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
@@ -17,10 +18,33 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    role: {
+      type: String,
+      enum: ["admin", "usuario"],
+      default: "usuario",
+    },
   },
   {
     timestamps: true,
   }
 );
+
+// Middleware para encriptar la contraseña antes de guardar
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    try {
+      const salt = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.password, salt);
+    } catch (error) {
+      return next(error);
+    }
+  }
+  next();
+});
+
+// Método para comparar contraseñas
+userSchema.methods.isValidPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 export default mongoose.model("User", userSchema);
---
+++ backend/src/routes/admin.routes.js
@@ -0,0 +1,21 @@
+// routes/admin.routes.js
+import { Router } from "express";
+import {
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+  createUser, // importa el nuevo controlador
+} from "../controllers/admin.controller.js";
+import { authRequired } from "../middlewares/validateToken.js";
+import { isAdmin } from "../middlewares/isAdmin.js";
+
+const router = Router();
+
+router.get("/admin/users", authRequired, isAdmin, getAllUsers);
+router.get("/admin/users/:id", authRequired, isAdmin, getUserById);
+router.post("/admin/users", authRequired, isAdmin, createUser); // nueva ruta
+router.put("/admin/users/:id", authRequired, isAdmin, updateUser);
+router.delete("/admin/users/:id", authRequired, isAdmin, deleteUser);
+
+export default router;
