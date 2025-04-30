c974cfdeb6923c36de7cf5e0cc0f1ff441a8f9e3
HulkyZeus <u20212200102@usco.edu.co>
2025-04-30 16:25:53 -0500

    Avances login y register, backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,10 +9,24 @@ import pedidosRoutes from "./routes/pedidos.routes.js";
 import reviewRoutes from "./routes/review.routes.js"
 import eventosRoutes from "./routes/eventos.routes.js"
 import inventarioRoutes from "./routes/inventario.routes.js";
+import cors from "cors";
+
+
 const app = express();
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
+
+//conexion a la base de datos
+connectDB();
+
+//Dependencias
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
+
+//Rutas
 app.use("/api", authRoutes);
 app.use("/api", taskRoutes);
 app.use("/api", pedidosRoutes);
@@ -20,7 +34,6 @@ app.use("/api", reviewRoutes);
 app.use("/api", eventosRoutes);
 app.use("/api", inventarioRoutes);
 
-connectDB();
-
+//Conexión al servidor
 app.listen(4000);
-console.log("Servidor corriendo", 4000);
+console.log("Servidor corriendo en el puerto: ", 4000);
